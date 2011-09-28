1d1865a7a22eea345ed5ff11fd32c258e74d6b5b
James S White <james@jameswhite.org>
2011-09-28 11:40:25 -0500

    dereferencing templates

--- lib/Nagios/Config/Intelligent.pm
+++ lib/Nagios/Config/Intelligent.pm
@@ -258,7 +258,6 @@ sub detemplate{
     my $type = shift;
     my $entry = shift;
     return $entry unless(defined($entry->{'use'}));
-    print STDERR "detemplating $type::$entry with $entry->{'use'}";
     my $template = $self->find_object($type,{ use => $entry->{'use'} });
     warn "no such template: $template\n" unless(defined($template));
     return $entry unless(defined($template));
@@ -277,7 +276,7 @@ sub find_object{
     my $attrs = shift if @_;  # a hash of the attributes that *all* must match to return the entry/entries
     my $records = undef;      # the list we'll be returning
     foreach my $entry (@{ $self->{'objects'}->{$type} }){
-        $entry = $self->detemplate($type, $entry);
+        $entry = $self->detemplate($type, $entry) if (defined($entry->{'use'}));
         my $allmatch=1;       # assume everything matches
         foreach my $needle (keys(%{ $attrs })){
             if(defined($entry->{$needle})){
