1a970048522591260cfd454369bb0f8795792700
James S White <james@jameswhite.org>
2011-09-29 09:24:09 -0500

    merging two sets of tools

--- lib/Nagios/Config/Intelligent.pm
+++ lib/Nagios/Config/Intelligent.pm
@@ -299,14 +299,6 @@ sub find_objects{
     my $attrs = shift if @_;  # a hash of the attributes that *all* must match to return the entry/entries
     my $records = undef;      # the list we'll be returning
     foreach my $entry (@{ $self->{'objects'}->{$type} }){
-        # replace expanded entry with the fully_expanded template;
-        if(defined($entry->{'use'})){
-            print STDERR "expanding ".$self->entry_name($entry)." with ".$entry->{'use'}."\n";
-            #print STDERR Data::Dumper->Dump(['before',$entry]);
-            $entry = $self->detemplate($type, $entry);
-            #print STDERR Data::Dumper->Dump(['after',$entry]);
-        }
-        sleep(1);
         my $allmatch=1;       # assume everything matches
         foreach my $needle (keys(%{ $attrs })){
             if(defined($entry->{$needle})){
