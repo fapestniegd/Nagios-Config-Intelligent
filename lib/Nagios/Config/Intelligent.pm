b17d41ac0f10642a6e3fd808944d9a80ad7f3c34
James S White <james@jameswhite.org>
2011-09-30 09:34:50 -0500

    de-templating work

--- lib/Nagios/Config/Intelligent.pm
+++ lib/Nagios/Config/Intelligent.pm
@@ -488,7 +488,8 @@ sub reduce {
     my $self = shift;
     my $type = shift;
     return undef unless $type;
-    my $sets = $self->clone($self->{'objects'}->{$type});
+print STDERR "-=[$type]=-\n";
+    my $sets $self->{'objects'}->{ $type };
     my $template_candidates;
     for(my $i=0; $i<=$#{$sets};$i++){
         for(my $j=0; $j<=$i;$j++){
