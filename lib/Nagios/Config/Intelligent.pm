22ef22c7185557b0ede7111ffb3360c62072d4e9
James S White <james@jameswhite.org>
2011-10-03 09:56:07 -0500

    more templating work

--- lib/Nagios/Config/Intelligent.pm
+++ lib/Nagios/Config/Intelligent.pm
@@ -595,7 +595,7 @@ sub reduce {
            }
         }
         # at this point we should have the entry, and the template it can be reduced by ind $tpl_name
-print STDERR Data::Dumper->Dump({ 'biggest_name' => $biggest_name });
+print STDERR Data::Dumper->Dump([{ 'biggest_name' => $biggest_name }]);
         if(defined($biggest_name)){
             foreach my $tplkey (keys(%{ $self->{'templates'}->{$type}->{$biggest_name} })){
                 delete $object_entry->{$tplkey} if(defined($object_entry->{$tplkey}));
