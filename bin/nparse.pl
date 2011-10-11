57a1bff785f5e3378492a1e7db951e2f60992871
James S White <james@jameswhite.org>
2011-10-11 15:52:22 -0500

    getting IPs from DNS

--- bin/nparse.pl
+++ bin/nparse.pl
@@ -29,8 +29,7 @@ my $n = Nagios::Config::Intelligent->new({
                                            'routers' => $routers,
                                         });
 
-my $g = Graph::Network->new({ 'routers' => $routers });
-$g->draw("routers.png");
+$n->{'g'}->draw("routers.png");
 #print Data::Dumper->Dump(['result',$n->find_object('host',{ 'alias' => 'skrs0019' }) ]);
 #print Data::Dumper->Dump([$n->intersection($n->{'objects'}->{'contact'}) ]);
 
