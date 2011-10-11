e3a804fd84b2ac9aba85c99382ac9dbefd66004a
James S White <james@jameswhite.org>
2011-10-11 09:31:50 -0500

    getting the graph working

--- bin/nparse.pl
+++ bin/nparse.pl
@@ -19,20 +19,25 @@ use Data::Dumper;
 my $result = GetOptions(
   'help'              => \$opt->{'help'},
   'config=s'          => \$opt->{'config'},
-  'topology=s'        => \$opt->{'topology'},
+  'routers=s'        => \$opt->{'routers'},
 );
 
 my $main_config = $opt->{'config'}  ||"/etc/nagios/nagios.cfg";
-my $topology    = $opt->{'topology'}||"/root/ncfg/etc/toplogy";
+my $routers    = $opt->{'routers'}||"/root/ncfg/etc/routers.cfg";
 my $n = Nagios::Config::Intelligent->new({
                                            'cfg'      => $main_config,
-                                           'topology' => $topology,
+                                           'routers' => $routers,
                                         });
 
 #print Data::Dumper->Dump(['result',$n->find_object('host',{ 'alias' => 'skrs0019' }) ]);
-
 #print Data::Dumper->Dump([$n->intersection($n->{'objects'}->{'contact'}) ]);
-$n->reduce_objects; # this is computationally expensive
-#$n->reduce('contact');
-print $n->dump();
-print $n->write_object_cfgs({ 'dir' => '/tmp/nagios.d/'});
+
+################################################################################
+# template reduction routines
+# $n->reduce_objects; # this is computationally expensive
+# #$n->reduce('contact');
+
+# print $n->dump();
+
+# $n->write_object_cfgs({ 'dir' => '/tmp/nagios.d/'});
+my $n = Graph::Network->new({ 'routers' => $routers });
