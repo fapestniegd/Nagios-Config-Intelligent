ac49b9ae92a0e9ef6b54119c7166f82d2f43e7ae
James S White <james@jameswhite.org>
2011-10-12 14:32:18 -0500

    getting IPs from DNS

--- lib/Graph/Network.pm
+++ lib/Graph/Network.pm
@@ -112,19 +112,16 @@ sub add_host{
             print STDERR "Unable to determine numeric IP for address [$hostdata->{'address'}]. It will be omitted from the graph.\n"; 
         }
     }
-    ############################################################################
-    # If the device is a soekris or a printer, we need to add it's /30 to networks
-    #
-    if($hostdata->{'name'} =~m /^(skrs|prnt)[0-9]{4}.cao/){
-          my ($interface_ip,$netbits) = split(/\//,"$hostdata->{'address'}/30");
-          my ($network, $broadcast)=split(/-/,join('',Net::CIDR::cidr2range("$hostdata->{'address'}/30")));
-          push(@{ $self->{'networks'} }, $network."/".$netbits);
-    }
-    #
-    ############################################################################
-    ############################################################################
-    # If the device is a soekris, we need to "vpn" it into 
-    #
+#    ############################################################################
+#    # If the device is a soekris or a printer, we need to add it's /30 to networks
+#    #
+#    if($hostdata->{'name'} =~m /^(skrs|prnt)[0-9]{4}.cao/){
+#          my ($interface_ip,$netbits) = split(/\//,"$hostdata->{'address'}/30");
+#          my ($network, $broadcast)=split(/-/,join('',Net::CIDR::cidr2range("$hostdata->{'address'}/30")));
+#          push(@{ $self->{'networks'} }, $network."/".$netbits);
+#    }
+#    #
+#    ############################################################################
     foreach my $cidr (@{ $self->{'networks'} }){
         if( Net::CIDR::cidrlookup($hostdata->{'address'}, $cidr) ){
              $self->{'g'}->add_vertex($cidr) unless $self->{'g'}->has_vertex($cidr);
