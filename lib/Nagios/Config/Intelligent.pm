89146a0dfaa468f1f82d1afe7838962c9eb0270a
James S White <james@jameswhite.org>
2011-10-14 11:59:05 -0500

    finding the closest polling server

--- lib/Nagios/Config/Intelligent.pm
+++ lib/Nagios/Config/Intelligent.pm
@@ -46,9 +46,9 @@ sub new{
     }
 
     # how our nagios servrers are layed out { 'report' => $report, 'poll' => [ $poll[1] .. $poll[n] ] }
+print Data::Dumper->Dump([ $cnstr ]);
     if(defined($cnstr->{'topology'})){
         $self->{'nagios'} = YAML::LoadFile($cnstr->{'toplogy'});
-print Data::Dumper->Dump([ $cnstr ]);
         foreach my $host (@{ $self->{'nagios'}->{'poll'} }){
             $self->{'g'}->add_host({ 'name'    => $host, 'address' => $host }); # get the poll servers on the graph
         }
