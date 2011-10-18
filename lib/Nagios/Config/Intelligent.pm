92346ac029a08b48a14366fb70acb1883383bd24
James S White <james@jameswhite.org>
2011-10-18 15:03:09 -0500

    expanding hostgroups into individual service checks

--- lib/Nagios/Config/Intelligent.pm
+++ lib/Nagios/Config/Intelligent.pm
@@ -299,7 +299,8 @@ sub write_object_cfgs{
                            $max_key_length=length($key) if(length($key) > $max_key_length);
                         }
                     }
-                    #if(defined(keys(%{ $self->{'templates'}->{ $object_type } }))){
+                    if(defined(keys(%{ $self->{'templates'}->{ $object_type } }))){
+print Data::Dumper->Dump([keys(%{ $self->{'templates'}->{ $object_type } })]);
                     #    print $fh "################################################################################\n";
                     #    print $fh "# Templates                                                                    #\n\n";
                     #    foreach my $template_name (keys(%{ $self->{'templates'}->{ $object_type } })){
@@ -313,7 +314,7 @@ sub write_object_cfgs{
                     #    }
                     #    print $fh "#                                                                              #\n";
                     #    print $fh "################################################################################\n\n";
-                    #}
+                    }
                     print $fh "################################################################################\n";
                     print $fh "# Objects                                                                      #\n\n";
                     foreach my $object (@{ $self->{'objects'}->{ $object_type } }){
