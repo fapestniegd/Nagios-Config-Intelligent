c13919da4a880ab6bbaace3fe1acd501e432147d
James S White <james@jameswhite.org>
2011-10-18 15:04:38 -0500

    expanding hostgroups into individual service checks

--- lib/Nagios/Config/Intelligent.pm
+++ lib/Nagios/Config/Intelligent.pm
@@ -301,19 +301,19 @@ sub write_object_cfgs{
                     }
                     if(defined(keys(%{ $self->{'templates'}->{ $object_type } }))){
 print STDERR Data::Dumper->Dump([keys(%{ $self->{'templates'}->{ $object_type } })]);
-                    #    print $fh "################################################################################\n";
-                    #    print $fh "# Templates                                                                    #\n\n";
-                    #    foreach my $template_name (keys(%{ $self->{'templates'}->{ $object_type } })){
-                    #        print $fh "define $object_type {\n";
-                    #        foreach my $key (keys(%{ $self->{'templates'}->{ $object_type }->{ $template_name } })){
-                    #            print $fh "    $key";
-                    #            for(my $i=0; $i<=$max_key_length-length($key); $i++){ print $fh " "; }
-                    #            print $fh "$self->{'templates'}->{ $object_type }->{ $template_name }->{$key}\n";
-                    #        }
-                    #        print $fh "}\n\n";
-                    #    }
-                    #    print $fh "#                                                                              #\n";
-                    #    print $fh "################################################################################\n\n";
+                        print $fh "################################################################################\n";
+                        print $fh "# Templates                                                                    #\n\n";
+                        foreach my $template_name (keys(%{ $self->{'templates'}->{ $object_type } })){
+                            print $fh "define $object_type {\n";
+                            foreach my $key (keys(%{ $self->{'templates'}->{ $object_type }->{ $template_name } })){
+                                print $fh "    $key";
+                                for(my $i=0; $i<=$max_key_length-length($key); $i++){ print $fh " "; }
+                                print $fh "$self->{'templates'}->{ $object_type }->{ $template_name }->{$key}\n";
+                            }
+                            print $fh "}\n\n";
+                        }
+                        print $fh "#                                                                              #\n";
+                        print $fh "################################################################################\n\n";
                     }
                     print $fh "################################################################################\n";
                     print $fh "# Objects                                                                      #\n\n";
