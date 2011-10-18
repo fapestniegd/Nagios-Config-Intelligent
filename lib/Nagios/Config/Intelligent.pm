8e0b3de6f0ea7f6abcc6cdbb38c4c2153e2ae544
James S White <james@jameswhite.org>
2011-10-18 09:18:02 -0500

    expanding hostgroups into individual service checks

--- lib/Nagios/Config/Intelligent.pm
+++ lib/Nagios/Config/Intelligent.pm
@@ -131,7 +131,7 @@ sub delegate {
                                                'hostgroup'      => $self->find_objects('hostgroup',{ 'hostgroup_name' => $service->{'hostgroup_name'} }),
                                                'members'        => [ $self->hostgroup_members($service->{'hostgroup_name'}) ],
                                             }]);
-            my $members = $self->hostgroup_members($service->{'hostgroup_name'});
+            my @{ $members } = @{ $self->hostgroup_members($service->{'hostgroup_name'}) };
             next unless $members;
             foreach my $host (@{ $members }){
                 my $new_service = $self->clone($service); 
