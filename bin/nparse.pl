f3483734bf6c28cf4f7748c1efe409392ae59090
James S White <james@jameswhite.org>
2011-10-19 11:57:35 -0500

    expanding hostgroups into individual service checks

--- bin/nparse.pl
+++ bin/nparse.pl
@@ -3,11 +3,11 @@ BEGIN {
         # our lib path is ../lib from this script add it to @INC 
         use Cwd; 
         use File::Basename; 
-        my $oldpwd=getcwd;
+        my $oldpwd = getcwd;
         chdir(dirname($0));      my $parent=getcwd;
         chdir(dirname($parent)); my $gparent=getcwd;
         unshift(@INC,"$gparent/lib") if(-d "$gparent/lib");
-        chdir( $oldpwd ); 
+        chdir( $oldpwd )if(defined($oldpwd)); 
       };
 
 use Getopt::Long;
